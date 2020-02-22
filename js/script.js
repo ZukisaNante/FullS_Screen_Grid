5bb71d5e011aab269fa18263d760fc60e2f97d31
Zukisa Nante <zukisanante@gmail.com>
2020-02-22 21:38:30 +0100

    100 cells completed but height not yet

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ function createGrid(x) {
             $(".container").append("<div class='grid'></div>");
         }
     }
-    $(".grid").width(960 / x);
-    $(".grid").height(960 / x);
+    $(".grid").width(960/x);
+    $(".grid").height(960/x);
 }
 
 // function that clears the grid
@@ -26,7 +26,7 @@ function refreshGrid() {
 // creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // allows the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
-    createGrid(16);
+    createGrid(50);
 
     $(".grid").mouseover(function() {
         $(this).css("background-color", "black");
